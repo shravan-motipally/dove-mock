37014a0424604ff54a9deddbcd4f64b484ce1c59
Shravan Motipally <numtheory@ymail.com>
2018-06-17 17:27:14 -0400

    @smotipally Added minor changes

--- src/js/components/About.js
+++ src/js/components/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from './Image';
 import Box from './Box';
 import MenuBar from './MenuBar';
 
@@ -14,20 +15,40 @@ const About = () => (
             <div className="row-fluid">
                 <div className="container-fluid">
                     <div className="row-fluid">
-                        <Box width={100} height={15} widthUnit={"%"} heightUnit={"vh"}/>
-                        <div className='rightPanel span6 offset3'>
+                        <div className='rightPanel span9 offset3'>
                             <div className="row-fluid bodyHeader">
                                 <h1><span className="welcomeHeaderText">About</span></h1>
+                                <Box width={100} height={1} widthUnit={"%"} heightUnit={"vh"}/>
+                            </div>
+                            <div className="row-fluid">
                                 <Box width={100} height={5} widthUnit={"%"} heightUnit={"vh"}/>
                             </div>
                             <div className="row-fluid">
-                                <p className="bodyText">
-                                    Greetings traveller! Thank you for visiting my site.
-                                    <br/>
-                                    My name is Shravan Motipally.  One of my many goals is to photograph this entire world.
-                                    <br/>
-                                    Feel free to check out the photos page to explore my adventures!
-                                </p>
+                                <div className="span3">
+                                    <Image imgIdx={6} />
+                                </div>
+                                <div className="span6">
+                                    <p className="bodyText">
+                                        Greetings traveller! Thank you for visiting my site.
+                                        <br/>
+                                        My name is Shravan Motipally.  One of my many goals is to photograph this entire world.
+                                        Feel free to check out the photos page to explore my adventures!
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="row-fluid">
+                                <Box width={100} height={5} widthUnit={"%"} heightUnit={"vh"}/>
+                            </div>
+                            <div className="row-fluid">
+                                <div className="span3">
+                                    <Image imgIdx={15} />
+                                </div>
+                                <div className="span6">
+                                    <p className="bodyText">
+                                        While you're there, buy your favorite pictures to support my adventure.
+                                        Each photo sold will come with a free choice of frame!  So shop now!
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
